12abd6e1c480a0d090f1aacc6194c191a3366663
Vince Patruno <patrunomeister@gmail.com>
2026-05-05 17:43:13 +0200

    tiendas

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         proyecto: resolve(__dirname, 'proyecto.html'),
+        contactos: resolve(__dirname, 'contactos.html'),
       },
     },
   },
